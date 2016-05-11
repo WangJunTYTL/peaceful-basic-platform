de17625cdae1c5ca382ef2b4e0e5e2b8b238e8e8
wangjun <wangjun@edaijia-inc.cn>
2016-05-11 17:23:07 +0800

    add classloader util

---
+++ peaceful-common-utils/src/main/java/com/peaceful/common/util/ClassUtils.java
@@ -0,0 +1,37 @@
+package com.peaceful.common.util;
+
+/**
+ * @author WangJun
+ * @version 1.0 16/5/11
+ */
+public class ClassUtils {
+
+
+    public static <T> T newInstance(String zClassName) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        return (T) getDefaultClassLoader().loadClass(zClassName).newInstance();
+    }""
+
+    public static ClassLoader getDefaultClassLoader() {
+        ClassLoader cl = null;
+        try {
+            cl = Thread.currentThread().getContextClassLoader();
+        }
+        catch (Throwable ex) {
+            // Cannot access thread context ClassLoader - falling back...
+        }
+        if (cl == null) {
+            // No thread context class loader -> use class loader of this class.
+            cl = ClassUtils.class.getClassLoader();
+            if (cl == null) {
+                // getClassLoader() returning null indicates the bootstrap ClassLoader
+                try {
+                    cl = ClassLoader.getSystemClassLoader();
+                }
+                catch (Throwable ex) {
+                    // Cannot access system ClassLoader - oh well, maybe the caller can live with null...
+                }
+            }
+        }
+        return cl;
+    }
+}
