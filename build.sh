7fb497227350ede4e87745594918ae50e6a2050d
Jun <wang@JundeMacBook-Pro.local>
2017-01-16 20:41:15 +0800

    fix commom utils build error

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 
 echo "该项目是基础依赖项目，有新的项目需要立项时只需依赖该项目可以帮助你快速集成项目的开发环境"
 
-echo "install peaceful-boot-parent pom"
+echo "install parent pom"
 mvn clean install -f ./peaceful-boot-parent/pom.xml || exit 1
+mvn clean install -f ./peaceful-parent/pom.xml || exit 1
 
 echo 'install peaceful basic jar'
 mvn clean install -f ./peaceful-boot/pom.xml
